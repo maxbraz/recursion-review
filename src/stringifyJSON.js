40b00b8b1535189373d7c31fe2f560c5011fc4cf
Hack Reactor Student <student@hackreactor.com>
2017-03-27 12:32:12 -0700

    finished up to stringify array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //find out the data type number, strings, boolean, function, undefined, and null
+  if (typeof(obj) === 'number') {
+  	return obj.toString();
+  }
+
+  if (obj === null) {
+  	return '' + obj;
+  }
+
+  if (typeof(obj) === 'boolean') {
+  	return obj.toString();
+  }
+
+  if (typeof(obj) === 'string') {
+  	return '"' + obj + '"';
+  }
+
+  //if the constructor is an array
+  if (Array.isArray(obj)) {
+  	var tempArray = [];
+
+  	obj.forEach(function(element) {
+  		tempArray.push(stringifyJSON(element))
+  	});
+
+  	return '[' + tempArray.toString() + ']';
+  }
+
+  //if the constructor is an object
+  if (obj.constructor === Object) {
+  	var objectString = '{';
+  	console.log(objectString)
+
+
+
+  	for (var key in obj) {
+  		if (key === undefined) {
+  			return '{}';
+  		}
+  		console.log(key)
+  		objectString += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+  	}
+
+  	objectString = objectString.slice(0, -1);
+  	return objectString
+  }
 };
