c3f847b8e21c5be2a394c97404f4a851f4065d72
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:43:04 -0700

    started parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,40 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  
+  var determineDataType = function(str) {
+  	if (str === "null") {
+  		return null;
+  	}
+
+  	if ((str === "true") || (str === "false")) {
+  		return Boolean(str);
+  	} 
+  }
+
+
+  var findAllIndices = function(str, char) {
+    var indicesOfChar = [];
+    var charArray = str.split('');
+    _.each(charArray, function(x, i) {
+    	if (x === char) {
+          indicesOfChar.push(i); 
+    	}
+    });
+    return indicesOfChar;
+  }  
+  
+  // if see '[' convert to array
+  if (json.indexOf('[') !== -1) {
+  	var arrContainer = [];
+  	if (findAllIndices(json, ',') === []) {
+      // pass
+  	}
+  	return arrContainer;
+  }
+
+  // of see '{' convert to object
+
+  
+  // if see '"' convert to string 
 };
